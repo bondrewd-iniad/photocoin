7ee09cfbc53b9b871b6226d581ab9ba6bf0221e9
bondrewd-iniad <bondrewd-iniad@users.noreply.github.com>
2023-06-27 08:48:47 +0000

    deploy: ad4ea74b6102287cb3d9689c51730ae144803661

---
+++ _next/static/RiyKwUJrz4KDNz7KaCi5q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-78c958dddd18195b.js","static/chunks/945-ee973bf32ee15d0f.js","static/chunks/pages/index-366f5f3e4cb5d467.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
